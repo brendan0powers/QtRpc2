dc5ec4dc5a2e99479485d6ea419d13689c2557b6
Brendan Powers <bpowers@ics.com>
2012-11-16 14:59:18 -0500

    examples/md5_*: Added an MD5 authentication example.

---
+++ examples/md5auth_client/basicservice.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *  Copyright (c) 2010, Resara LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Resara LLC nor the
+ *       names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL RESARA LLC BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***************************************************************************/
+#ifndef BASICSERVICE_H
+#define BASICSERVICE_H
+
+#include <ClientProxy>
+
+using namespace QtRpc;
+
+//Client service inherit from ClientProxy
+//Inheriting from another client service is also allowed
+class BasicService : public ClientProxy
+{
+    Q_OBJECT
+public:
+    explicit BasicService(QObject *parent = 0);
+    
+    //Functions to be called over the network are signals with
+    //a return value of ReturnValue. Running or calling this signal
+    //Will call the remote services function, and return the result.
+    //
+    //Note that signals have been redeclared public, so you can call them
+    //from outside the class...
+signals:
+    ReturnValue addNumbers(int a, int b);
+    ReturnValue returnError();
+    ReturnValue salt();
+    ReturnValue authenticate(const QString &hash);
+};
+
+#endif // BASICSERVICE_H
---
+++ examples/md5auth_client/main.cpp
@@ -0,0 +1,102 @@
+/***************************************************************************
+ *  Copyright (c) 2010, Resara LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Resara LLC nor the
+ *       names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL RESARA LLC BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***************************************************************************/
+#include <QApplication>
+#include <QDebug>
+#include <QCryptographicHash>
+#include "basicservice.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc,argv);
+
+    //Create an instance of the service object
+    BasicService service;
+
+    //Connect to the server, specifying the port, and the remove service to connect to
+    //Also specifying a user name.
+    ReturnValue ret = service.connect("tcp://jon@localhost:10123/MyService");
+    //If the connection failes, ret.isError() will be true
+    if(ret.isError())
+    {
+        //You can use ReturnValues in qDebug() statements to quickly print error messages.
+        qCritical() << "Failed to connect:" << ret;
+        return(1);
+    }
+
+    //Retrive the salt from the server
+    ret = service.salt();
+    if(ret.isError())
+    {
+        qCritical() << "Failed to call salt():" << ret;
+        return(1);
+    }
+
+    //Compute the MD5 hash from the salt
+    QString salt = ret.toString();
+    QString hash = QCryptographicHash::hash(QString(salt+"secret").toAscii(), QCryptographicHash::Md5).toHex();
+
+    qDebug() << "Salt:" << salt;
+    qDebug() << "Hash:" << hash;
+
+    //Use the hash to authenticate
+    ret = service.authenticate(hash);
+    if(ret.isError())
+    {
+        qCritical() << "Failed to authenticate:" << ret;
+        return(1);
+    }
+
+    qDebug() << "Authentication Succeeded";
+
+    //Run the remove function. This will block untill the function returns,
+    //Or untill the default time-out is reached
+    ret = service.addNumbers(3,5);
+    if(ret.isError())
+    {
+        qCritical() << "Failed to call addNumbers():" << ret;
+        return(1);
+    }
+
+    //You can use ReturnValue just like a QVariant
+    qDebug() << "Call to add() succeeded.";
+    qDebug() << "Result:" << ret.toInt();
+    qDebug();
+
+    //Calling this function will return an error.
+    ret = service.returnError();
+    if(ret.isError())
+    {
+        //The ReturnValue class can be used to inspect the error.
+        qDebug() << "Failed to call returnError():" << ret;
+        qDebug() << "Error Number:" << ret.errNumber();
+        qDebug() << "Error String:" << ret.errString();
+        qDebug() << "This was expected";
+    }
+
+    return(0);
+}
